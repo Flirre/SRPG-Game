f65d9cee4d8a84214574ba1e47b7ffe4ac0e40da
Filip Lindahl <Gysztaf@gmail.com>
2016-02-17 12:42:51 +0100

    Added a bunch of enums. And wrote the debug constructor slightly more efficient.

--- src/com/company/Character.java
+++ src/com/company/Character.java
@@ -16,9 +16,11 @@ public class Character {
     private int magic;
     private int magicDefence;
     private boolean isDead;
+    private int xpos;
+    private int ypos;
 
 
-    public Character(String name, Job job, int level, int health, int mp, int attack, int defence, int speed, int magic, int magicDefence, boolean isDead) {
+    public Character(String name, Job job, int level, int health, int mp, int attack, int defence, int speed, int magic, int magicDefence, boolean isDead, int xpos, int ypos) {
         this.name = name;
         this.job = job;
         this.level = level;
@@ -30,20 +32,13 @@ public class Character {
         this.magic = magic;
         this.magicDefence = magicDefence;
         this.isDead = isDead;
+        this.xpos = xpos;
+        this.ypos = ypos;
+
     }
 
     public Character(String name){
-        this.name = name;
-        this.job = Job.Warrior;
-        this.level = 1;
-        this.health = 5;
-        this.mp = 5;
-        this.attack = 10;
-        this.defence = 10;
-        this.speed = 4;
-        this.magic = 10;
-        this.magicDefence = 10;
-        this.isDead = false;
+        this(name, Job.Warrior, 1, 1, 1, 1, 1, 1, 1, 1, false, 0, 0);
     }
 
     public int getHealth() {
@@ -122,12 +117,52 @@ public class Character {
         return this.isDead;
     }
 
-    public void setDead(){
+    public void setDead() {
         if (getHealth()<1){
             this.isDead = true;
         }
     }
+
+    public int getXpos() {
+        return this.xpos;
+    }
+
+    public void setXpos(int newXpos) {
+        this.xpos = newXpos;
+    }
+
+    public int getYpos() {
+        return this.ypos;
+    }
+
+    public void setYpos(int newYpos) {
+        this.ypos = newYpos;
+    }
     public enum Job {
         Warrior, Archer
+    }
+
+    public enum Right_Hand {
+        Nothing, Small_sword, Big_sword, Small_bow, Big_bow
+    }
+
+    public enum Left_Hand {
+        Nothing, Small_shield, Big_shield, Dagger, Wand
+    }
+
+    public enum Headgear {
+        Nothing, Helmet
+    }
+
+    public enum Torso {
+        Nothing, Leather_armor
+    }
+
+    public enum Legs {
+        Nothing, Leather_greaves
+    }
+
+    public enum Shoes {
+        Nothing, Leather_shoes
     }
 }
