a0f58305ff4424a1b789665bf2b617efddc7bcc6
Filip Lindahl <gysztaf@gmail.com>
2016-06-12 15:01:20 +0200

    Added a couple of constructors and a "ToString".

--- src/com/company/Tile.java
+++ src/com/company/Tile.java
@@ -6,4 +6,23 @@ package com.company;
 public class Tile {
     private int zpos;
     private String terrain;
+
+    public Tile (String terrain) {
+        this.zpos = 0;
+        this.terrain = terrain;
+    }
+
+    public Tile(int zpos, String terrain) {
+        this.zpos = zpos;
+        this.terrain = terrain;
+    }
+
+    public Tile() {
+        this.zpos = 0;
+        this.terrain = "Grass";
+    }
+    public String ToString() {
+    return this.terrain; //change this to first letter of the type of terrain later.
+    }
+
 }
