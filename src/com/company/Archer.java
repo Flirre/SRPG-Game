d2ddf0bedd6adbf4cf0074a4a5516fa5f05042d5
Filip Lindahl <gysztaf@gmail.com>
2016-06-14 09:02:42 +0200

    Added an archer. And his bow.

---
+++ src/com/company/Archer.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by flirre on 13/06/16.
+ */
+public class Archer extends Job {
+    public Archer () {
+        super(8, 2, 4, 3, 8, 1, 4);
+    }
+}
---
+++ src/com/company/Bow.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by flirre on 13/06/16.
+ */
+public class Bow extends Weapon {
+
+    public Bow (String name, int weight, int durability, int damage, int range) {
+        super(name, weight, durability, damage, range);
+    }
+}
